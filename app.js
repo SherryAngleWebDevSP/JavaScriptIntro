737710b38189889d57cd3ee89ec4cc6ce5a345bc
Sherry Angle <SherryAngleWD@gmail.com>
2022-07-19 15:52:21 -0500

    Final Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+console.log(`JavaScriptIntro`);
+
+
+const FirstName = Sherry;
+let LastName = Angle;
+var age = 58;
+
+LastName = "Triangle";
+age= 48;
+
+
+let language = "JavaScript";
+let CreatedYear = 1955;
+let isCaseSensitive = true;
+
+console.log(`{language} was first release in ${CreatedYear}.`);
+
+let product= "T-Shirt";
+let price = 19.99;
+let isOnSale= false;
+let SalePercentage = 15%;
+let stock = 0;
+let InStock = false;
+let SelectedSize= "m";
+
+console.log(
+    `${product} in size ${SelectedSize} is ${price} with ${stock} in stock.`
+        );
+
+let title = "Nmae of the Wind";
+let author = "Patrick Rothfuss";
+let PageCount = 722;
+let bookmark = "start";
+let HasRead = false;
+
+console.log(
+    `${title} by ${author} is ${PageCount} pages long. Your last bookmark was page ${bookmark}.`
+);
+
+
+
+
